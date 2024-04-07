1b0e0a20b7284fb145d9baf7b0cdbd0f1a9f47c9
ResearcherXman <xhs.research@gmail.com>
2024-04-07 18:44:30 +0800

    support image-based stylization

---
+++ infer_style_controlnet.py
@@ -0,0 +1,54 @@
+import torch
+from diffusers import ControlNetModel, StableDiffusionXLControlNetPipeline
+
+import cv2
+from PIL import Image
+
+from ip_adapter import IPAdapterXL
+
+base_model_path = "stabilityai/stable-diffusion-xl-base-1.0"
+image_encoder_path = "sdxl_models/image_encoder"
+ip_ckpt = "sdxl_models/ip-adapter_sdxl.bin"
+device = "cuda"
+
+controlnet_path = "diffusers/controlnet-canny-sdxl-1.0"
+controlnet = ControlNetModel.from_pretrained(controlnet_path, use_safetensors=False, torch_dtype=torch.float16).to(device)
+
+# load SDXL pipeline
+pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+    base_model_path,
+    controlnet=controlnet,
+    torch_dtype=torch.float16,
+    add_watermarker=False,
+)
+
+# load ip-adapter
+# target_blocks=["block"] for original IP-Adapter
+# target_blocks=["up_blocks.0.attentions.1"] for style blocks only
+# target_blocks = ["up_blocks.0.attentions.1", "down_blocks.2.attentions.1"] # for style+layout blocks
+ip_model = IPAdapterXL(pipe, image_encoder_path, ip_ckpt, device, target_blocks=["up_blocks.0.attentions.1"])
+
+# style image
+image = "./assets/4.jpg"
+image = Image.open(image)
+image.resize((512, 512))
+
+# control image
+input_image = cv2.imread("./assets/yann-lecun.jpg")
+detected_map = cv2.Canny(input_image, 50, 200)
+canny_map = Image.fromarray(cv2.cvtColor(detected_map, cv2.COLOR_BGR2RGB))
+
+# generate image
+images = ip_model.generate(pil_image=image,
+                           prompt="a man, masterpiece, best quality, high quality",
+                           negative_prompt= "text, watermark, lowres, low quality, worst quality, deformed, glitch, low contrast, noisy, saturation, blurry",
+                           scale=1.0,
+                           guidance_scale=5,
+                           num_samples=1,
+                           num_inference_steps=30, 
+                           seed=42,
+                           image=canny_map,
+                           controlnet_conditioning_scale=0.6,
+                          )
+
+images[0].save("result.png")
