0eeaeaa86fec8cf5676e512ae073cda7e7afb258
linoytsaban <linoy@huggingface.co>
2024-04-09 12:41:34 +0300

    fixes

--- ip_adapter/ip_adapter_faceid.py
+++ ip_adapter/ip_adapter_faceid.py
@@ -7,6 +7,8 @@ from diffusers.pipelines.controlnet import MultiControlNetModel
 from PIL import Image
 from safetensors import safe_open
 from transformers import CLIPImageProcessor, CLIPVisionModelWithProjection
+
+from .attention_processor_faceid import LoRAAttnProcessor, LoRAIPAttnProcessor
 from .utils import is_torch2_available, get_generator
 
 USE_DAFAULT_ATTN = False  # should be True for visualization_attnmap
@@ -112,48 +114,6 @@ class ProjPlusModel(torch.nn.Module):
         if shortcut:
             out = x + scale * out
         return out
-
-
-class ImageProjModel(torch.nn.Module):
-    """Projection Model"""
-
-    def __init__(self, cross_attention_dim=1024, clip_embeddings_dim=1024, clip_extra_context_tokens=4):
-        super().__init__()
-
-        self.generator = None
-        self.cross_attention_dim = cross_attention_dim
-        self.clip_extra_context_tokens = clip_extra_context_tokens
-        self.proj = torch.nn.Linear(clip_embeddings_dim, self.clip_extra_context_tokens * cross_attention_dim)
-        self.norm = torch.nn.LayerNorm(cross_attention_dim)
-
-    def forward(self, image_embeds):
-        embeds = image_embeds
-        clip_extra_context_tokens = self.proj(embeds).reshape(
-            -1, self.clip_extra_context_tokens, self.cross_attention_dim
-        )
-        clip_extra_context_tokens = self.norm(clip_extra_context_tokens)
-        return clip_extra_context_tokens
-
-
-class MLPProjModel(torch.nn.Module):
-    def __init__(self, cross_attention_dim=768, id_embeddings_dim=512, num_tokens=4):
-        super().__init__()
-
-        self.cross_attention_dim = cross_attention_dim
-        self.num_tokens = num_tokens
-
-        self.proj = torch.nn.Sequential(
-            torch.nn.Linear(id_embeddings_dim, id_embeddings_dim * 2),
-            torch.nn.GELU(),
-            torch.nn.Linear(id_embeddings_dim * 2, cross_attention_dim * num_tokens),
-        )
-        self.norm = torch.nn.LayerNorm(cross_attention_dim)
-
-    def forward(self, id_embeds):
-        x = self.proj(id_embeds)
-        x = x.reshape(-1, self.num_tokens, self.cross_attention_dim)
-        x = self.norm(x)
-        return x
 
 
 class IPAdapterFaceID:
@@ -270,6 +230,7 @@ class IPAdapterFaceID:
             **kwargs,
     ):
         self.set_scale(scale)
+
         num_prompts = faceid_embeds.size(0)
 
         if prompt is None:
@@ -638,6 +599,7 @@ class IPAdapterFaceIDPlus:
             s_scale=1.0,
             shortcut=False,
             **kwargs,
+
     ):
         self.set_scale(scale)
 
